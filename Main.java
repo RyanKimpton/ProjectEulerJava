33276d97889ffea08d74341e330f496cc56e4bb3
Ryan Kimpton <RyanKimpton965@gmail.com>
2019-10-02 09:17:10 +0100

    First commit

--- Main.java
+++ Main.java
@@ -1,8 +1,6 @@
 import java.lang.Math;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+
 
 public class Main {
 
@@ -12,7 +10,12 @@ public class Main {
             return true;
         }
 
-        for(int i = 2; i < Math.sqrt(numb)+1; i++){
+        if( numb % 2 == 0){
+            return false;
+        }
+
+
+        for(int i = 3; i < Math.sqrt(numb)+1; i += 2){
             if(numb%i == 0){
                 return false;
             }
@@ -106,6 +109,31 @@ public class Main {
 
         return (int) lcm;
 
+    }
+
+
+    private static List primeSieve(int max){
+        List primes = new ArrayList<Integer>();
+        boolean prime[] = new boolean[max+1];
+
+        for(int i=0;i<max;i++)
+            prime[i] = true;
+
+        for(int p = 2; p*p <=max; p++){
+            if(prime[p] == true){
+                for(int i = p*p; i <= max; i += p) {
+                    prime[i] = false;
+                }
+            }
+        }
+
+        for(int i = 2; i <= max; i++) {
+            if(prime[i] == true){
+                primes.add(i);
+            }
+        }
+
+        return primes;
     }
 
 
@@ -270,14 +298,93 @@ public class Main {
         return (int) (a*b*c);
     }
 
+    private static long problemTen(){
+        int max = 2000000;
+        long sum = 0;
+        List<Integer> primes = new ArrayList();
 
+        primes = primeSieve(max);
+
+        for(int i = 0; i < primes.size(); i++){
+            sum += primes.get(i);
+        }
+
+        return sum;
+    }
+
+    private static int problemEleven(){
+        String bigNum = "08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08 " +
+                "49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48 04 56 62 00 " +
+                "81 49 31 73 55 79 14 29 93 71 40 67 53 88 30 03 49 13 36 65 " +
+                "52 70 95 23 04 60 11 42 69 24 68 56 01 32 56 71 37 02 36 91 " +
+                "22 31 16 71 51 67 63 89 41 92 36 54 22 40 40 28 66 33 13 80 " +
+                "24 47 32 60 99 03 45 02 44 75 33 53 78 36 84 20 35 17 12 50 " +
+                "32 98 81 28 64 23 67 10 26 38 40 67 59 54 70 66 18 38 64 70 " +
+                "67 26 20 68 02 62 12 20 95 63 94 39 63 08 40 91 66 49 94 21 " +
+                "24 55 58 05 66 73 99 26 97 17 78 78 96 83 14 88 34 89 63 72 " +
+                "21 36 23 09 75 00 76 44 20 45 35 14 00 61 33 97 34 31 33 95 " +
+                "78 17 53 28 22 75 31 67 15 94 03 80 04 62 16 14 09 53 56 92 " +
+                "16 39 05 42 96 35 31 47 55 58 88 24 00 17 54 24 36 29 85 57 " +
+                "86 56 00 48 35 71 89 07 05 44 44 37 44 60 21 58 51 54 17 58 " +
+                "19 80 81 68 05 94 47 69 28 73 92 13 86 52 17 77 04 89 55 40 " +
+                "04 52 08 83 97 35 99 16 07 97 57 32 16 26 26 79 33 27 98 66 " +
+                "88 36 68 87 57 62 20 72 03 46 33 67 46 55 12 32 63 93 53 69 " +
+                "04 42 16 73 38 25 39 11 24 94 72 18 08 46 29 32 40 62 76 36 " +
+                "20 69 36 41 72 30 23 88 34 62 99 69 82 67 59 85 74 04 36 16 " +
+                "20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54 " +
+                "01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48";
+        String[] doubles = bigNum.split(" ");
+        int max = 0;
+        int current = 0;
+        int index;
+
+        //Row start
+        for( int i = 0; i < 20; i++) {
+            //col start
+            for (int j = 0; j < 20; j++) {
+                index = i + 20 * j;
+                //Decreasing diag:
+                if (i < 17 & j < 17) {
+                    current = Integer.parseInt(doubles[index]) * Integer.parseInt(doubles[index + 21]) * Integer.parseInt(doubles[index + 42]) * Integer.parseInt(doubles[index + 63]);
+                    if (current > max) {
+                        max = current;
+                    }
+                }
+                //Increasing diag:
+                if (i < 17 & j > 2) {
+                    current = Integer.parseInt(doubles[index]) * Integer.parseInt(doubles[index - 19]) * Integer.parseInt(doubles[index - 38]) * Integer.parseInt(doubles[index - 57]);
+                    if (current > max) {
+                        max = current;
+                    }
+                }
+                //Top to bottom:
+                if (j < 17) {
+                    current = Integer.parseInt(doubles[index]) * Integer.parseInt(doubles[index + 20]) * Integer.parseInt(doubles[index + 40]) * Integer.parseInt(doubles[index + 60]);
+                    if (current > max) {
+                        max = current;
+                    }
+                }
+                //Left to right
+                if (i < 17) {
+                    current = Integer.parseInt(doubles[index]) * Integer.parseInt(doubles[index + 1]) * Integer.parseInt(doubles[index + 2]) * Integer.parseInt(doubles[index + 3]);
+                    if (current > max) {
+                        max = current;
+                    }
+                }
+            }
+        }
+        return max;
+    }
+
+    private static int problemTwleve(){
+        
+    }
 
 
 
 
     public static void main(String[] args){
         long startTime = System.nanoTime();
-
 
         System.out.println("problem 1 = " + problemOne());
         System.out.println("problem 2 = " + problemTwo());
@@ -288,6 +395,9 @@ public class Main {
         System.out.println("Problem 7 = " + problemSeven());
         System.out.println("Problem 8 = " + problemEight());
         System.out.println("Problem 9 = " + problemNine());
+        System.out.println("Problem 10 = " + problemTen());
+        System.out.println("Problem 11 = " + problemEleven());
+
 
 
         long endTime = System.nanoTime();
